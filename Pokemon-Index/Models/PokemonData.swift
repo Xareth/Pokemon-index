37dda4298bb615451a209b7cbcd32b39379411a9
Xareth <w6white@gmail.com>
2020-05-06 13:07:04 +0200

    JSON parse

---
+++ Pokemon-Index/Models/PokemonData.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonData.swift
+//  Pokemon-Index
+//
+//  Created by Mikołaj Szadkowski on 06/05/2020.
+//  Copyright © 2020 Mikołaj Szadkowski. All rights reserved.
+//
+
+import Foundation
+
+struct PokemonData: Codable {
+    var name: String
+}
