5992ced5054c153606e1d98202262314ff3ccd01
Xareth <w6white@gmail.com>
2020-05-06 10:58:58 +0200

    Initial Commit

---
+++ Pokemon-Index/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon-Index
+//
+//  Created by Mikołaj Szadkowski on 06/05/2020.
+//  Copyright © 2020 Mikołaj Szadkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
